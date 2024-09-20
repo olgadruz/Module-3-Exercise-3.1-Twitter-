8749a6f78715e9cd8db8833d27540e2d4f2d8600
Оля Дружкова <Druzhkova@MacBook-Pro-Ola.local>
2024-09-20 15:28:51 -0400

    first draft

---
+++ Twitter.cs
@@ -0,0 +1,18 @@
+﻿namespace Twitter;
+
+class Twitter
+{
+    static void Main(string[] args)
+    {
+        string message = Console.ReadLine();
+        int g = message.Length;
+        if (g > 140)
+        {
+            Console.WriteLine("Rejected");
+        }
+        else
+        {
+            Console.WriteLine("Posted");
+        }
+    }
+}
